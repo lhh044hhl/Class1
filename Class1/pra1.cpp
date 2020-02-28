d09c6a5ccd6848c6fed8668eca5b654a352928f6
113295634@qq.com <113295634@qq.com>
2020-02-28 14:55:40 +0800

    练习1-3

---
+++ Class1/pra1.cpp
@@ -0,0 +1,62 @@
+#include <opencv.hpp>
+
+using namespace cv;
+//Á·Ï°Ò»
+/*
+int main()
+{
+	cv::Mat srcMat = imread("E:\\dignal picture\\1.jpg");
+		int height = srcMat.rows;
+	    int width = srcMat.cols;
+
+	for (int j = 0;j <height;j++) {
+
+		for (int i = 0;i < width;i++) {
+			uchar average = (srcMat.at<Vec3b>(j, i)[0] + srcMat.at<Vec3b>(j, i)[1] + srcMat.at<Vec3b>(j, i)[2]) / 3;
+			srcMat.at<Vec3b>(j, i)[0] = average;
+				srcMat.at<Vec3b>(j, i)[1] = average;
+				srcMat.at<Vec3b>(j, i)[2] = average;
+		}
+			
+
+	}
+	imshow("src", srcMat);
+	waitKey(0);
+}
+*/
+
+//Á·Ï°¶þ
+/*
+int main() {
+
+	Mat gray =imread("E:\\dignal picture\\1.jpg" , 0);
+	imshow("gray", gray);
+		waitKey(0);
+}
+*/
+
+//Á·Ï°Èý
+int main()
+{
+	cv::Mat srcMat = imread("E:\\dignal picture\\1.jpg");
+	int height = srcMat.rows;
+	int width  = srcMat.cols;
+	uchar threshold = 100;
+	for (int j = 0;j < height;j++) {
+
+		for (int i = 0;i < width;i++) {
+			uchar average = (srcMat.at<Vec3b>(j, i)[0] + srcMat.at<Vec3b>(j, i)[1] + srcMat.at<Vec3b>(j, i)[2]) / 3;
+			if (average > threshold)
+				average == 255;
+			else
+				average == 0;
+			srcMat.at<Vec3b>(j, i)[0] = average;
+			srcMat.at<Vec3b>(j, i)[1] = average;
+			srcMat.at<Vec3b>(j, i)[2] = average;
+		}
+
+
+	}
+	imshow("src", srcMat);
+	waitKey(0);
+}
