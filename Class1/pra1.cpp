b6c20665aafdd51efb0a31c89959a54e4a08e545
113295634@qq.com <113295634@qq.com>
2020-02-28 15:11:03 +0800

    练习1-4

--- Class1/pra1.cpp
+++ Class1/pra1.cpp
@@ -62,3 +62,31 @@ int main()
 	waitKey(0);
 }
 */
+//Á·Ï°ËÄ
+int main() {
+	Mat srcMat = imread("E:\\dignal picture\\1.jpg");
+	Mat shallowMat = srcMat;
+	Mat deepMat;
+	srcMat.copyTo(deepMat);
+	int height = srcMat.rows;
+	int width = srcMat.cols;
+	uchar threshold = 100;
+	for (int j = 0;j < height;j++) {
+
+		for (int i = 0;i < width;i++) {
+			uchar average = (srcMat.at<Vec3b>(j, i)[0] + srcMat.at<Vec3b>(j, i)[1] + srcMat.at<Vec3b>(j, i)[2]) / 3;
+			if (average > threshold)
+				average == 255;
+			else
+				average == 0;
+			srcMat.at<Vec3b>(j, i)[0] = average;
+			srcMat.at<Vec3b>(j, i)[1] = average;
+			srcMat.at<Vec3b>(j, i)[2] = average;
+		}
+
+
+	}
+	imshow("deep", deepMat);
+	imshow("shallow", shallowMat);
+	waitKey(0);
+}
